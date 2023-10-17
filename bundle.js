989e195a6fa8cb59d0618f13359b3219cb5e67c2
Minjae <68741986+Shampo0@users.noreply.github.com>
2023-10-17 17:38:53 +0900

    mouse pointer

--- bundle.js
+++ bundle.js
@@ -96,171 +96,275 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var cytoscape_cose_bilkent__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(cytoscape_cose_bilkent__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(76);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_2__);
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 
 
 cytoscape__WEBPACK_IMPORTED_MODULE_0___default.a.use(cytoscape_cose_bilkent__WEBPACK_IMPORTED_MODULE_1___default.a);
 
+// webpack으로 묶어줘야 하니 css파일을 진입점인 index.js 에 import 합니다
 var data = [{
-  "data": {
-    "id": "robotncom",
-    "url": "http://www.robotncom.com/",
-    "label": "robotncom"
+  data: {
+    id: "robotncom",
+    url: "",
+    label: "robotncom"
   }
 }, {
-  "data": {
-    "id": "history",
+  data: {
+    id: "platform",
+    label: "platform"
   }
 }, {
-  "data": {
-    "id": "robotncom->history",
-    "source": "history",
-    "target": "robotncom"
-  }
-},
-// {
-//     "data": {
-//         "id": "logo",
-//     }
-// },
-// {
-//     "data": { "id": "history->logo", "source": "logo", "target": "history" }
-// },
-{
-  "data": {
-    "id": "platform"
+  data: {
+    id: "robotncom->platform",
+    source: "platform",
+    target: "robotncom"
   }
 }, {
-  "data": {
-    "id": "robotncom->platform",
-    "source": "platform",
-    "target": "robotncom"
+  data: {
+    id: "taktaguri",
+    label: "taktaguri"
   }
 }, {
-  "data": {
-    "id": "taktaguri"
+  data: {
+    id: "naafaa",
+    label: "naafaa"
   }
 }, {
-  "data": {
-    "id": "platform->taktaguri",
-    "source": "taktaguri",
-    "target": "platform"
+  data: {
+    id: "platform->taktaguri",
+    source: "taktaguri",
+    target: "platform"
   }
 }, {
-  "data": {
-    "id": "naafaa",
-    "url": "https://naafaa.co.kr/"
+  data: {
+    id: "platform->naafaa",
+    source: "naafaa",
+    target: "platform"
   }
 }, {
-  "data": {
-    "id": "platform->naafaa",
-    "source": "naafaa",
-    "target": "platform"
+  data: {
+    id: "solution",
+    label: "solution"
   }
 }, {
-  "data": {
-    "id": "solution"
+  data: {
+    id: "robotncom->solution",
+    source: "solution",
+    target: "robotncom"
   }
 }, {
-  "data": {
-    "id": "robotncom->solution",
-    "source": "solution",
-    "target": "robotncom"
+  data: {
+    id: "sever",
+    label: "sever"
   }
 }, {
-  "data": {
-    "id": "sever"
+  data: {
+    id: "solution->sever",
+    source: "sever",
+    target: "solution"
   }
 }, {
-  "data": {
-    "id": "security"
+  data: {
+    id: "security",
+    label: "security"
   }
 }, {
-  "data": {
-    "id": "network"
+  data: {
+    id: "solution->security",
+    source: "security",
+    target: "solution"
   }
 }, {
-  "data": {
-    "id": "solution->sever",
-    "source": "sever",
-    "target": "solution"
+  data: {
+    id: "network",
+    label: "network"
   }
 }, {
-  "data": {
-    "id": "solution->security",
-    "source": "security",
-    "target": "solution"
+  data: {
+    id: "solution->network",
+    source: "network",
+    target: "solution"
   }
 }, {
-  "data": {
-    "id": "solution->network",
-    "source": "network",
-    "target": "solution"
+  data: {
+    id: "history",
+    label: "history"
   }
 }, {
-  "data": {
-    "id": "computer HW + SW"
+  data: {
+    id: "robotncom->history",
+    source: "history",
+    target: "robotncom"
   }
 }, {
-  "data": {
-    "id": "robotncom->computer HW + SW",
-    "source": "computer HW + SW",
-    "target": "robotncom"
+  data: {
+    id: "computer",
+    label: "computer HW + SW"
   }
-}
-// {
-//     "data": {
-//         "id": "STUDY-webpackBuild",
-//     }
-// },
-// {
-//     "data": { "id": "computer HW + SW->STUDY-webpackBuild", "source": "STUDY-webpackBuild", "target": "computer HW + SW" }
-// },
-// {
-//     "data": {
-//         "id": "STUDY-jsBrowser",
-//     }
-// },
-// {
-//     "data": { "id": "STUDY-webpackBuild->STUDY-jsBrowser", "source": "STUDY-jsBrowser", "target": "STUDY-webpackBuild" }
-// }
-];
-// 공식 사이트 예제 코드
-var cy = cytoscape__WEBPACK_IMPORTED_MODULE_0___default()({
-  container: document.getElementById('cy'),
-  // container to render in
+}, {
+  data: {
+    id: "robotncom->computer",
+    source: "computer",
+    target: "robotncom"
+  }
+}];
+var cy_for_rank = cytoscape__WEBPACK_IMPORTED_MODULE_0___default()({
+  elements: data
+});
+// rank를 활용하기 위해 data만 입력한 cytoscape 객체입니다
 
+var pageRank = cy_for_rank.elements().pageRank();
+// elements들의 rank들입니다.
+
+var nodeMaxSize = 50;
+var nodeMinSize = 5;
+var nodeActiveSize = 28;
+var fontMaxSize = 8;
+var fontMinSize = 5;
+// 추후 마우스 인/아웃 시에도 활용해야 하니 node와 font의 최대값/최소값은 변수로 빼줍니다
+var fontActiveSize = 7;
+// node & font 크기 값
+
+var edgeWidth = '2px';
+var edgeActiveWidth = '4px';
+var arrowScale = 0.8;
+var arrowActiveScale = 1.2;
+// edge & arrow 크기값
+
+var dimColor = '#dfe4ea';
+var edgeColor = '#ced6e0';
+var nodeColor = '#57606f';
+var nodeActiveColor = '#ffa502';
+var successorColor = '#ff6348';
+// 상위 node & edge color
+var predecessorsColor = '#1e90ff';
+// 하위 node & edge color
+var cy = cytoscape__WEBPACK_IMPORTED_MODULE_0___default()(_defineProperty({
+  container: document.getElementById("cy"),
   elements: data,
-  style: [
-  // the stylesheet for the graph
-  {
-    selector: 'node',
+  style: [{
+    selector: "node",
     style: {
-      'background-color': '#666',
-      'label': 'data(id)'
+      'background-color': nodeColor,
+      label: "data(label)",
+      width: function width(ele) {
+        return nodeMaxSize * pageRank.rank("#" + ele.id()) + nodeMinSize;
+      },
+      height: function height(ele) {
+        return nodeMaxSize * pageRank.rank("#" + ele.id()) + nodeMinSize;
+      },
+      "font-size": function fontSize(ele) {
+        return fontMaxSize * pageRank.rank("#" + ele.id()) + fontMinSize;
+      }
     }
   }, {
-    selector: 'edge',
+    selector: "edge",
     style: {
-      'width': 3,
-      'curve-style': 'bezier',
-      'line-color': '#ccc',
-      'source-arrow-color': '#ccc',
-      'source-arrow-shape': 'triangle'
+      'width': edgeWidth,
+      'line-color': edgeColor,
+      'source-arrow-color': edgeColor,
+      'source-arrow-shape': 'vee',
+      'arrow-scale': arrowScale
     }
   }],
   layout: {
-    name: 'cose-bilkent',
+    name: "cose-bilkent",
     animate: false,
     gravityRangeCompound: 1.5,
     fit: true,
     tile: true
   }
-});
-cy.on('tap', function (e) {
-  var url = e.target.data('url');
-  if (url && url !== '') {
+}, "layout", {
+  name: "cose-bilkent",
+  animate: false,
+  gravityRangeCompound: 1.5,
+  fit: true,
+  tile: true
+}));
+function setDimStyle(target_cy, style) {
+  target_cy.nodes().forEach(function (target) {
+    target.style(style);
+  });
+  target_cy.edges().forEach(function (target) {
+    target.style(style);
+  });
+}
+function setFocus(target_element, successorColor, predecessorsColor, edgeWidth, arrowScale) {
+  target_element.style('background-color', nodeActiveColor);
+  target_element.style('color', nodeColor);
+  target_element.successors().each(function (e) {
+    // 상위  엣지와 노드
+    if (e.isEdge()) {
+      e.style('width', edgeWidth);
+      e.style('arrow-scale', arrowScale);
+    }
+    e.style('color', nodeColor);
+    e.style('background-color', successorColor);
+    e.style('line-color', successorColor);
+    e.style('source-arrow-color', successorColor);
+    setOpacityElement(e, 0.5);
+  });
+  target_element.predecessors().each(function (e) {
+    // 하위 엣지와 노드
+    if (e.isEdge()) {
+      e.style('width', edgeWidth);
+      e.style('arrow-scale', arrowScale);
+    }
+    e.style('color', nodeColor);
+    e.style('background-color', predecessorsColor);
+    e.style('line-color', predecessorsColor);
+    e.style('source-arrow-color', predecessorsColor);
+    setOpacityElement(e, 0.5);
+  });
+  target_element.neighborhood().each(function (e) {
+    // 이웃한 엣지와 노드
+    setOpacityElement(e, 1);
+  });
+  target_element.style('width', Math.max(parseFloat(target_element.style('width')), nodeActiveSize));
+  target_element.style('height', Math.max(parseFloat(target_element.style('height')), nodeActiveSize));
+  target_element.style('font-size', Math.max(parseFloat(target_element.style('font-size')), fontActiveSize));
+}
+function setOpacityElement(target_element, degree) {
+  target_element.style('opacity', degree);
+}
+function setResetFocus(target_cy) {
+  target_cy.nodes().forEach(function (target) {
+    target.style('background-color', nodeColor);
+    var rank = pageRank.rank(target);
+    target.style('width', nodeMaxSize * rank + nodeMinSize);
+    target.style('height', nodeMaxSize * rank + nodeMinSize);
+    target.style('font-size', fontMaxSize * rank + fontMinSize);
+    target.style('color', nodeColor);
+    target.style('opacity', 1);
+  });
+  target_cy.edges().forEach(function (target) {
+    target.style('line-color', edgeColor);
+    target.style('source-arrow-color', edgeColor);
+    target.style('width', edgeWidth);
+    target.style('arrow-scale', arrowScale);
+    target.style('opacity', 1);
+  });
+}
+cy.on("tap", function (e) {
+  var url = e.target.data("url");
+  if (url && url !== "") {
     window.open(url);
   }
+});
+cy.on('tapstart mouseover', 'node', function (e) {
+  setDimStyle(cy, {
+    'background-color': dimColor,
+    'line-color': dimColor,
+    'source-arrow-color': dimColor,
+    'color': dimColor
+  });
+  setFocus(e.target, successorColor, predecessorsColor, edgeActiveWidth, arrowActiveScale);
+});
+cy.on('tapend mouseout', 'node', function (e) {
+  setResetFocus(e.cy);
 });
 
 /***/ }),
